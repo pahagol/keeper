521f9207defcb5009470f3397134c64870e7b662
pasha <p.golovenko@tehnomir.com.ua>
2014-12-26 14:00:55 +0200

    work with sum month price and view.js and add.js remake on widget ajax and work with event

---
+++ js/widget/event.js
@@ -0,0 +1,18 @@
+(function(keeper) {
+
+keeper.widgetEvent = {
+
+	handler: {},
+	
+	// выполнить событие
+	dispatchEvent: function(event) {
+		
+	},
+	
+	// навесить событие
+	handleEvent: function(target, event, callback) {
+		
+	}
+};
+
+}(keeper));
